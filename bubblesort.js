431bc1236ba591a83a385fb09b5ae59b66454408
Alex Bukhman <alexb823@gmail.com>
2019-01-16 20:42:59 -0500

    added nested for loops solution

--- bubblesort.js
+++ bubblesort.js
@@ -1,20 +1,41 @@
+//Solution 1 using while with a for loop
+// function bubbleSort(array) {
+//   const arrayCopy = [...array];
+//   let lastUnsortedIndex = arrayCopy.length - 1;
+//   let sorted = false;
+
+//   while (!sorted) {
+//     sorted = true;
+//     for (let i = 0; i < lastUnsortedIndex; ++i) {
+//       const currentVal = arrayCopy[i];
+//       const nextVal = arrayCopy[i + 1];
+//       if (currentVal > nextVal) {
+//         arrayCopy[i] = nextVal;
+//         arrayCopy[i + 1] = currentVal;
+//         sorted = false; //if swapped, array was not sorted
+//       }
+//     }
+//     lastUnsortedIndex -= 1;
+//   }
+//   return arrayCopy;
+// }
+
+//Solution 2 using nested for loops
 function bubbleSort(array) {
   const arrayCopy = [...array];
-  let lastUnsortedIndex = arrayCopy.length - 1;
-  let sorted = false;
 
-  while (!sorted) {
-    sorted = true;
-    for (let i = 0; i < lastUnsortedIndex; ++i) {
-      const currentVal = arrayCopy[i];
-      const nextVal = arrayCopy[i + 1];
+  for(let i = arrayCopy.length - 1; i > 0; --i) {
+    let sorted = true;
+    for(let j = 0; j < i; ++j) {
+      let currentVal = arrayCopy[j];
+      let nextVal = arrayCopy[j + 1];
       if (currentVal > nextVal) {
-        arrayCopy[i] = nextVal;
-        arrayCopy[i + 1] = currentVal;
-        sorted = false; //if swapped array was not sorted
+        arrayCopy[j] = nextVal;
+        arrayCopy[j + 1] = currentVal;
+        sorted = false; //if swapped, array was not sorted
       }
     }
-    lastUnsortedIndex -= 1;
+    if(sorted) return arrayCopy;
   }
   return arrayCopy;
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,7 +1,9 @@
-describe('Bubble Sort', function(){
-  it('handles an empty array', function(){
-    expect( bubbleSort([]) ).toEqual( [] );
+describe('Bubble Sort', function() {
+  const numArr = [1, 3, 5, 2, 4, 6, 10, 7, 8, 9];
+  it('handles an empty array', function() {
+    expect(bubbleSort([])).toEqual([]);
+  });
+  it('sorts an array of numbers', function() {
+    expect(bubbleSort(numArr)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
   });
 });
-
-
